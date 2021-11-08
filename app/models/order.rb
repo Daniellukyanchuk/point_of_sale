6a9c11f82c73df4a1db3a132c68f21c9e82a0cf5
Daniellukyanchuk <daniellukyanchuk@gmail.com>
2021-11-08 15:45:10 +0600

    the default dates on the datepicker fields are showing up now

--- app/models/order.rb
+++ app/models/order.rb
@@ -93,7 +93,9 @@ class Order < ApplicationRecord
       else
         product_id_where = "(product_id IN (#{product_ids.join(", ")}) )"
       end
-
+      
+      date_filter_where = "WHERE order_products.created_at >= cast(now() as date) - interval '30' day"
+      
       if start_date.blank? || end_date.blank?
         date_filter_where = "WHERE order_products.created_at >= cast(now() as date) - interval '30' day" 
       else
@@ -120,7 +122,7 @@ class Order < ApplicationRecord
            ORDER BY #{sortable} #{sort_direction}
                       
         """
-        
+
       result = ActiveRecord::Base.connection.execute(sql)
     end
 end
