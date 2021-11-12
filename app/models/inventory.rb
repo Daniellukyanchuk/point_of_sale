6336242b5a02c64212badc624fc5a8aaeb20ac81
Daniellukyanchuk <daniellukyanchuk@gmail.com>
2021-11-12 12:59:49 +0600

    now I can search multiple items in the inventory table

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -3,13 +3,23 @@ class Inventory < ApplicationRecord
 	before_save :set_costs
 	belongs_to :order_product
 
-	def self.search(search, select_product)
+	def self.search(search)
 	  if !search.blank?
 	  	return Inventory.joins(:product).where("products.product_name ilike? or product_id = ?", "%#{search.strip}%", search.to_i)
 	  else
 	  	return Inventory.joins(:product).all
 	  end
 	end
+
+	def self.product_select(product_select)
+	  if !product_select.blank?
+	  	return Inventory.joins(:product).where('products.product_name IN (?) OR product_id IN (?)', "%#{product_select}%", product_select)	  	
+	  else 
+	  	return Inventory.joins(:product).all		
+	  end
+	end
+
+	
 
 	def current_amount_left
 		self.current_amount_left = amount 
