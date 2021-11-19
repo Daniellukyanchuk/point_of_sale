a797bc7023addd981e6b50a25edbbb7cf1561046
Daniellukyanchuk <daniellukyanchuk@gmail.com>
2021-11-19 16:18:16 +0600

    Order_product & inventory relation is now working fine!

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -2,7 +2,7 @@ class OrderProduct < ApplicationRecord
   belongs_to :order
   validates :quantity, :sale_price, presence: true, length: { minimum: 1, maximum: 25}
   has_many :inventories
-  after_save :set_current_amount_left
+  before_save :set_current_amount_left
   validate :has_enough_inventory
     
   # num = 45
@@ -24,43 +24,40 @@ class OrderProduct < ApplicationRecord
       errors.add(:quantity, "Not enough inventory for this product")
     end
   end
-  # Should subtract quantity from inventories.current_amount_left.
-  # Should subtract from the oldest inventory if there is enough, if not, subtract what there is and subtract the rest
-  # from the next to the oldest inventory for the same product.   
+  
   def set_current_amount_left   
+    
+    # amount_left_to_remove should always be the amount of inventory that we still have to remove
+    change_in_quantity = self.quantity - (self.quantity_was || 0)
+    amount_left_to_remove = change_in_quantity
 
-    amount_left_to_remove = self.quantity
-
-
-    Inventory.where("product_id = ? and current_amount_left > 0", self.product_id).each do |inv|        
-      # amount_to_remove_from_current_inventory_row = 
+    # 1: Find out how much we need to remove from inventory by setting it equal to amount_bought.(What does it mean to set equal to amount_bought?)
+    # 2: Get the oldest muffin inventory with amount left.(How to do this?)
+    # 3: Find the smaller number between amount_left_to_remove and oldest_inventory.current_amount_left and store that as amount_to_remove.
+    # How to get the oldest_inventory.current_amount_left?
+    # 4: Subtract amount_to_remove from oldest_inv.current_amount_left and store it in current_amount_left.
+    # 5: Save the oldest_inv.current_amount_left.
+    # 6: Subtract amount_to_remove from amount_left_to_removed and store in amount_left_to_remove.
+    # 7: Now do it again.
+   
+    Inventory.where("product_id = ? and current_amount_left > 0", self.product_id).order(:created_at, :id).each do |inv|              
       
-      inv.current_amount_left = ?
-      inv.save
+      break if amount_left_to_remove == 0
 
-      inv.current_amount_left = inv.current_amount_left.to_d - self.quantity
+      # inv.current_amount_left, amount_left_remove
+      amount_to_remove = [inv.current_amount_left, amount_left_to_remove].min
+      
+      inv.current_amount_left = inv.current_amount_left - amount_to_remove
+      res = inv.save      
 
-      if inv.current_amount_left == self.quantity
-        inv.current_amount_left.order(:created_at, :id)   
-      inv.save            
-    end 
+      amount_left_to_remove = amount_left_to_remove - amount_to_remove
+    end     
   end
 end
-  # Do an .order after the where
+
+
   
-  # Variable Name Description
-  # Situation 1 Person buys 30 muffins  amount_bought 30
-  # What should be the end result?  25 muffins should be taken from the oldest muffin inventory row, 
-  # which is Inventory_id 1 and the next muffin row (2) should have 5 less     
-  # Step 1: Find out how much we need to remove_from inventory by setting it equal to amount_bought amount_left_to_remove 30
-  # Step 2: Get the oldest muffin inventory with amount left  oldest_inv  First inventory row
-  # Step 3: Find the smaller number between amount_left_to_remove and 
-  # oldest_inventory.current_amount_left and store that as amount_to_remove amount_to_remove   25
-  # Step 4: Subtract amount_to_remove from oldest_inv.current_amount_left and store it in 
-  # current_amount_left oldest_inv.current_amount_left   0
-  # Step 5: Save the oldest_inv Now the table shows 0 for inv 1 current_amount_left 0
-  # Step 6: Subtract amount_to_remove from amount_left_to_remobe and store in amount_left_to_remove amount_left_to_remove 5
-  # Now do it again   
+ 
 
 
 
