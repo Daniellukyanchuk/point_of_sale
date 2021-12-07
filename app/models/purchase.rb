c5d5b4666a8ac1210cc462e4100f86c9094ad58a
Daniellukyanchuk <daniellukyanchuk@gmail.com>
2021-12-07 15:16:34 +0600

    stuff is working now

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,5 +2,22 @@ class Purchase < ApplicationRecord
 	has_many :purchase_products
 	belongs_to :supplier
 	accepts_nested_attributes_for :purchase_products, allow_destroy: true
+	before_save :set_actual_total
+	before_save :set_estimated_total
 
+	def set_actual_total
+      self.actual_total = 0
+      purchase_products.each do |pp|
+        pp.set_estimated_subtotal
+        self.actual_total = self.actual_total + pp.estimated_subtotal
+      end
+    end    
+
+    def set_estimated_total
+      self.estimated_total = 0
+      purchase_products.each do |pp|
+    	pp.set_estimated_subtotal
+    	self.estimated_total = self.estimated_total + pp.estimated_subtotal
+      end
+    end
 end
--- app/models/purchase_product.rb
+++ app/models/purchase_product.rb
@@ -4,4 +4,8 @@ class PurchaseProduct < ApplicationRecord
   def set_estimated_subtotal
     self.estimated_subtotal = estimated_quantity * estimated_price_per_unit
   end
+
+  def set_actual_subtotal
+  	self.actual_subtotal = actual_quantity * actual_price_per_unit
+  end
 end
