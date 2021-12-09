a50b9596cddb5a24398cf4b88f5c5a92c5db9793
Daniellukyanchuk <daniellukyanchuk@gmail.com>
2021-12-09 17:08:31 +0600

    stuff is looking better now.

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ApplicationRecord
   has_many :orders, through: :order_products
   has_many :clients, through: :orders
   has_many :inventories  
+  has_many :purchase_products
+
   def self.search(search)
     if !search.blank?
       return Product.where("product_name ilike ? or id = ?", "%#{search.strip}%", search.to_i)    
--- app/models/purchase_product.rb
+++ app/models/purchase_product.rb
@@ -1,6 +1,7 @@
 class PurchaseProduct < ApplicationRecord
 	belongs_to :purchase
   has_many :inventories
+  belongs_to :product
 
   def set_estimated_subtotal
     self.estimated_subtotal = estimated_quantity * estimated_price_per_unit
