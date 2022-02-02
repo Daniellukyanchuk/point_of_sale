36612898a713c96e10fc62d7fbaf035dbf4d215d
Daniellukyanchuk <daniellukyanchuk@gmail.com>
2022-02-02 11:53:05 +0600

    RSpec

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -2,18 +2,13 @@ require 'rails_helper'
 # require 'inventory'
 
 RSpec.describe Inventory, type: :model do
- 
-    it 'adds correct amount to the inventory' do 
-      
+    it 'adds correct amount to the inventory' do       
       prod = Product.create(product_name: "muffin", price: 50, unit: "piece")
-
       # there are no muffins in inventory
-      expect()
 
-      inventory_add = Inventory.add_inventory(prod.id, ?, ?)
+      inventory_add = Inventory.add_inventory(prod.id, prod.amount, prod.recipe_price)
 
       # check to see that you have the exact number of added muffins in inventory
-      expect().to be > 0
+      expect(inventory_add).to eq(true) 
     end
-   
 end
