f6877885a90daf3f99d2562a7a03eaf4121ec564
quickfoxcreative <quickfoxcreative@gmail.com>
2022-02-07 15:51:38 +0600

    adding inventory summary (not finished)

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -54,11 +54,9 @@ def set_remaining_quantity_for_production
     remaining_quantity: remaining_quantity)    
   end
 
-  
-
   def self.search(search)
     if !search.blank?
-        return Inventory.where("id = ? or product_id = ? or purchase_order_id = ? or purchase_order_product_id = ?", "%#{search.strip}%", "%#{search.strip}%", search.to_i)
+        return Inventory.where("product_id = ? or product_name ILIKE ? or purchase_order_product_id = ?", "%#{search.strip}%", "%#{search.strip}%", search.to_i)
     else
     Inventory.all
     end
@@ -86,8 +84,63 @@ def set_remaining_quantity_for_production
           break amount_left_to_remove == 0
       end 
 
-      return amount_left_to_remove
-      alert("Insuficient Inventory")
-  end
 
+  def self.inventory_report(search_product, pick_product, datefilter_start, datefilter_end, sortable, sort_direction)
+stop
+      datefilter_start = Date.strptime( datefilter_start, '%m/%d/%Y')       
+      datefilter_end = Date.strptime( datefilter_end, '%m/%d/%Y')     
+
+
+#search box (search_product)
+
+      where_search = " (product_name ILIKE '%#{search_product}%' OR unit ILIKE '%#{search_product}%' OR product_id = #{search_product.to_i} 
+                      OR current_inventory = #{search_product.to_f} OR unit = #{search_product.to_f})"
+    
+      if search_product.blank?
+        where_search = ""
+      end  
+
+#product multi-select (pick_product)
+        
+      if pick_product.blank? || pick_product == [""] 
+        where_picker = ""
+      else
+        where_picker = "product_id IN (#{pick_product.join(",")})"
+      end
+
+#filter by time period (date_filter_start & _end)    
+
+      where_time_period = "WHERE inventories.created_at >= #{SqlHelper.escape_sql_param(datefilter_start.to_date)} AND inventories.created_at <= #{SqlHelper.escape_sql_param(datefilter_end.to_date)}"
+      
+      where_statement = WhereBuilder.build([where_search, where_picker])
+
+#column sorting (sortable & sort_direction)
+
+      if !["product_id", "product_name", "current_inventory", "unit"].include?(sortable)
+        sortable = "product_name"
+      end
+
+      if  sort_direction == "desc"
+        "desc"
+      else
+        sort_direction = "asc"
+      end
+        
+      sql = """
+        SELECT * FROM (
+          SELECT product_id, product_name, unit, SUM(remaining_quantity) AS current_inventory
+          FROM inventories
+          LEFT OUTER JOIN products ON product_id = products.id
+          #{where_time_period}
+          GROUP BY product_id, product_name, unit
+          ) report
+        #{where_statement}            
+        ORDER BY #{sortable} #{sort_direction}
+
+      """
+
+      result = ActiveRecord::Base.connection.execute(sql)      
+        
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/reports/inventory_report', to: 'reports#inventory_report'
   get '/recipe_products/recipe_info', to: 'products#get_recipe_info'
   get '/recipe/unit', to: 'products#get_unit'
   get '/recipe/production_info', to: 'recipes#get_production_info'
