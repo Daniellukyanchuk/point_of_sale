ea003571e47625ef711669642870ab190752bf50
Daniellukyanchuk <daniellukyanchuk@gmail.com>
2022-02-14 21:34:23 +0600

    RSpec

---
+++ app/models/my_sorter.rb
@@ -0,0 +1,33 @@
+class MySorter
+
+# Make a method sort_array 
+# Method takes in an array of either strings or integers, 
+# and returns an array with items sorted asc or desc. The default is ascending. 
+# You are not allowed to use rails sort method, you have to write your own. 
+
+  def self.sort_array(array)
+    # a = []
+    # a.push(min)
+    array.each do |a|
+      b += a.delete_at(a).get_min
+    end
+
+    
+
+  end
+
+  def self.get_min(min)
+    
+
+  end
+
+  # def sort_me(array)
+  # end
+ 
+
+
+
+
+
+
+end
---
+++ spec/models/my_sorter_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe MySorter, type: :model do
+
+  it "it takes an array of either integers or string and sorts them by default ascending" do  
+    aa = [9, 3, 6, 2, 5]
+
+   
+
+    b = MySorter.sort_array(aa)
+    # c = MySorter.new
+    # c.sort_me(aa)
+
+    expect(b).to eq([2, 3, 5, 6, 9])
+  end
+
+  it "takes an array and returns smallest element in the array" do  
+    
+    aa = [9, 3, 6, 2, 5]
+
+    b = MySorter.get_min(aa)
+
+    expect(b).to eq([2])
+   
+
+  end
+end
