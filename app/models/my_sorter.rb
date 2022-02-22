3d7b7bf876c0c78df953cf1cb305f4df7b43fe3c
quickfoxcreative <quickfoxcreative@gmail.com>
2022-02-22 16:10:47 +0600

    merge sort working

--- app/models/my_sorter.rb
+++ app/models/my_sorter.rb
@@ -1,8 +1,225 @@
 class MySorter < ApplicationRecord
 
-	def self.sort_array(array_of_strings_or_numbers)
+	def self.selection_sort(unsorted_array)
 
 		sorted_array = []
+		arr = unsorted_array		
+
+		while arr.length > 0 
+			sorted_array.push(get_min(arr))
+			arr.delete(get_min(arr))
+		end
+
+		return sorted_array
 	end
 
+	def self.get_min(some_array)
+
+		cur_min = nil
+		
+		some_array.each do |item|
+			if cur_min.nil?
+				cur_min = item
+			elsif cur_min > item
+				cur_min = item
+			end		
+		end
+		return cur_min
+	end 
+
+	def self.bubble_sort(unsorted_array)
+
+		arr = unsorted_array
+		n = arr.length	
+
+			loop do
+				changed = false	
+				
+				(n-1).times do |j| 
+					if 	arr[j] > arr[j + 1]
+						arr[j], arr[j + 1] = arr[j + 1], arr[j]
+						changed = true
+					end
+				end
+				break if not changed
+			end
+		arr
+	end
+
+	def self.value_sort(unsorted_hash)
+		selection_sort(unsorted_hash.values)		
+	end
+
+#   {c: 1, d: 5, e: 4, a: 2, b: 3}
+
+
+	def self.key_sort(unsorted_hash)
+
+		sorted_keys = selection_sort(unsorted_hash.keys)
+		sorted_values = []
+
+		sorted_keys.each do |sv|
+			sorted_values.push(unsorted_hash[sv])			
+		end
+		sorted_values
+	end
+
+
+	def self.reverse_string(some_string)
+
+		some_string = "zebra"
+		arr = some_string.chars 
+
+		reversed_string = []
+
+		position = arr.length - 1
+		while position >= 0 
+			reversed_string.push(arr[position])
+			position -= 1
+		end
+
+	# 	while arr.length > 0 
+	# 		reversed_string.push(arr.pop)
+	# 	end
+		reversed_string.join("")
+	end
+
+
+
+	# def self.find_deepest(arr, depth=0)
+	# 	dig_results = []
+	# 		arr.each do |fd|		
+	# 		dig_results.push(dig_deeper(arr, 0))
+	# 		end
+	# 	dig_results
+	# end
+
+	
+
+	def self.dig_deeper (arr, depth=0)
+
+		deepest = {depth: depth, values: []}
+
+			arr.each do |dd|
+
+				if dd.class == Array				
+					result = dig_deeper(dd, depth + 1)
+					if result[:depth] > deepest[:depth]
+						deepest = result
+					elsif result[:depth] = deepest[:depth]
+						deepest[:values] += result[:values]
+						
+					end
+				else
+					deepest[:values].push(dd)
+				end	
+				
+			end
+
+		return deepest
+		
+	end
+
+	def self.merge_sort(arr)
+
+		sorted_array = []
+
+		if arr.length > 1
+			split_arr = arr.in_groups(2, false)
+
+			left_sorted_array = merge_sort(split_arr[0])
+			right_sorted_array = merge_sort(split_arr[1])
+			
+			x = 0
+			j = 0
+			while x <= left_sorted_array.length - 1 && j <= right_sorted_array.length - 1
+
+				if left_sorted_array[x] <= right_sorted_array[j]
+					sorted_array.push(left_sorted_array[x])
+					x += 1
+				else 
+					sorted_array.push(right_sorted_array[j])
+					j += 1
+				end			
+			end
+
+			while j <= right_sorted_array.length - 1
+				sorted_array.push(right_sorted_array[j])
+				j += 1
+			end
+
+			while x <= left_sorted_array.length - 1
+				sorted_array.push(left_sorted_array[x])
+				x += 1
+			end
+
+		else 
+			sorted_array = arr
+		end
+		
+		return sorted_array
+	end
+
+	def self.quick_sort(arr)
+		left = 0
+		right = arr.length - 1
+		piv = arr.length/2
+		sorted_array = []
+
+		while left < piv && right > piv
+			if arr[left] >= arr[piv] && arr[right] <= arr[piv]
+				arr[left], arr[right] = arr[right], arr[left]
+				left  += 1
+				right -= 1
+			elsif arr[left] < arr[piv] 
+				left += 1
+			elsif arr[right] > arr[piv]
+				right -= 1
+			end			
+		end
+byebug
+		if left == piv || right == piv
+			split_arr = arr.in_groups(2, false)
+			left_sorted_array = quick_sort(split_arr[0])
+			right_sorted_array = quick_sort(split_arr[1])	
+		end
+byebug
+		x = 0
+		y = 0
+		while x < left_sorted_array.length  && y < right_sorted_array.length 
+
+			if left_sorted_array[x] <= right_sorted_array[y]
+				sorted_array.push(left_sorted_array[x])
+				x += 1
+			else 
+				sorted_array.push(right_sorted_array[y])
+				y += 1
+			end			
+		end
+
+		while y <= right_sorted_array.length - 1
+			sorted_array.push(right_sorted_array[y])
+			y += 1
+		end
+
+		while x <= left_sorted_array.length - 1
+			sorted_array.push(left_sorted_array[x])
+			x += 1
+		end
+
+		sorted_array = arr
+	end
 end
+
+# a clear exampe of what the method should return
+# how to reduce the problem (moving towards base case)	
+# how do you recognize when the problem can't be broken down anymore (how do you know you're at basecase)
+# how to merge the results
+
+# compare first item of left_sorted_array with first item of right_sorted_array
+# if left is smaller, add it to sorted array 
+# then compare second item in left to first item in right_sorted_array
+# if right is smaller, add it to sorted array
+
+
+
--- spec/models/my_sorter_spec.rb
+++ spec/models/my_sorter_spec.rb
@@ -1,14 +1,105 @@
 require 'rails_helper'
 
 RSpec.describe MySorter, type: :model do
-  it "should sort an array of items in ascending or descending order, ascending by default" do
+  it "should sort an array of items in ascending order" do
 
       #create mock array
       sample_array = [3, 8, 5, 2, 10]
 
       #call sort array method w/ sample data
-      MySorter.sort_array(sample_array)   
+      select_sorted = MySorter.selection_sort(sample_array) 
 
-      expect.sample_array.to eq([2, 3, 5, 8, 10])
+      expect(select_sorted).to eq([2, 3, 5, 8, 10])
+  end
+
+  it "should return the smallest number or string in an array" do
+
+      #create mock array
+      sample_array = [3, 8, 5, 2, 10]
+
+      #call sort array method w/ sample data
+      min = MySorter.get_min(sample_array) 
+
+      expect(min).to eq(2)
+  end
+
+  it "should sort an array of items in ascending order " do
+
+      #create mock array
+      sample_array = [3, 8, 5, 2, 10]
+
+      #call sort array method w/ sample data
+      bubble_sorted = MySorter.bubble_sort(sample_array) 
+
+      expect(bubble_sorted).to eq([2, 3, 5, 8, 10])
+  end
+
+  it "should sort the values of a hash by value in ascending order " do
+
+      #create mock array
+      sample_hash =  {a: 2, b: 3, c: 1, d: 5, e: 4}
+
+      #call sort array method w/ sample data
+      sorted_hash = MySorter.value_sort(sample_hash) 
+
+      expect(sorted_hash).to eq( [1,2,3,4,5])
+  end
+
+  it "should sort values of a hash by key of a hash in ascending order " do
+
+      #create mock array
+      sample_hash =  {c: 1, d: 5, e: 4, a: 2, b: 3} 
+
+      #call sort array method w/ sample data
+      key_sorted = MySorter.key_sort(sample_hash) 
+
+      expect(key_sorted).to eq( [2, 3, 1, 5, 4])
+  end
+
+  it "should reverse the order or the word" do
+
+      #create mock array
+      sample_word = "zebra"      
+
+      #call sort array method w/ sample data
+      reversed_word = MySorter.reverse_string(sample_word) 
+
+      expect(reversed_word).to eq("arbez")
+  end
+
+  it "should find the deepest element(s) and return its depth and value(s)" do
+
+      #create mock array
+      tmp = [['1a'], ['1b', ['2a']], ['1c', '1d', ['2b'], ['2c', '2d', ['3a']]], [[['3b']]]]
+      
+
+      #call sort array method w/ sample data
+      deepest_element = MySorter.dig_deeper(tmp)
+
+      expect(deepest_element).to eq({depth: 3, values: ["3a", "3b"]})
+  end
+
+  it "should sort the array in ascending order" do
+
+      #create mock array
+      tmp = [2,5,7,8,1,10,9,3,4,6]
+      
+
+      #call sort array method w/ sample data
+      sorted_array = MySorter.merge_sort(tmp)
+
+      expect(sorted_array).to eq([1,2,3,4,5,6,7,8,9,10])
+  end
+
+  it "should sort the array in ascending order" do
+
+      #create mock array
+      tmp = [2,10,7,8,1,4,9,3,5,6]
+      
+
+      #call sort array method w/ sample data
+      sorted_array = MySorter.quick_sort(tmp)
+
+      expect(sorted_array).to eq([1,2,3,4,5,6,7,8,9,10])
   end
 end
