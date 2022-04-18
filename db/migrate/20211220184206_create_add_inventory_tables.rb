485413e0be27c47b8bcc02da33b0b71ce106e7bb
quickfoxcreative <quickfoxcreative@gmail.com>
2022-04-18 16:37:39 +0600

    migration

--- db/migrate/20211220184206_create_add_inventory_tables.rb
+++ db/migrate/20211220184206_create_add_inventory_tables.rb
@@ -4,14 +4,14 @@ class CreateAddInventoryTables < ActiveRecord::Migration[5.2]
     remove_column :supply_products, :remaining_quantity, :float
   end
 
-  def change
-    create_table :inventory_records do |t|
-        t.integer :supply_id
-        t.integer :product_id
-        t.float :remaining_quantity
-        t.date :date_received
+  # def change
+  #   create_table :inventory_records do |t|
+  #       t.integer :supply_id
+  #       t.integer :product_id
+  #       t.float :remaining_quantity
+  #       t.date :date_received
 
-      t.timestamps
-    end
-  end
+  #     t.timestamps
+  #   end
+  # end
 end
