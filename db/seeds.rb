99db66fa42d389bde6d5ec3dcb421a1d0cbd3005
quickfoxcreative <quickfoxcreative@gmail.com>
2022-05-04 10:40:36 +0600

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 if User.count == 0
     Role.create(role_name: "System Admin")
-    User.create(email: "quickfoxcreative@gmail.com", password: "123456", role_users_attributes: [user_id: 1, role_id: 1]))
+    User.create(email: "quickfoxcreative@gmail.com", password: "123456", role_users_attributes: [user_id: 1, role_id: 1])
 end
 
 if Setting.count == 0
