2f0a521189b016782b182402b93ed69705d2faad
quickfoxcreative <quickfoxcreative@gmail.com>
2022-05-04 15:33:07 +0600

    ljdf

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-unless User.exists?(:user_name => "POS Admin")
-    #add admin role
+unless Role.exists?(:role_name => "System Admin")
+    #create admin role
     admin_role = Role.create(:role_name => "System Admin")
     #get id of admin role
     role_id = admin_role.id
+end
+    
+
+unless User.exists?(:user_name => "Admin")
     #create admin user
-    admin_user = User.create(:user_name => "POS Admin", :email => "quickfoxcreative@gmail.com", :password => "123456")
+    admin_user = User.create(:user_name => "Admin", :email => "quickfoxcreative@gmail.com", :password => "123456")
     #get id of admin user
     user_id = admin_user.id
+end
+
+unless RoleUser.exists?(:role_id => role_id, :user_id => user_id)    
     #assign role to user
     RoleUser.create(:user_id => user_id, :role_id => role_id)
 end
@@ -22,3 +29,5 @@ end
 if Setting.count == 0
     Setting.create(company_name: "Company Name", company_address: "Company Address", company_phone: "+999 (999) 999 999")
 end
+
+
