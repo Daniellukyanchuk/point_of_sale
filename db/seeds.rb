60022c07dd47a5cc268cd94f1bf0a638af22b9f3
quickfoxcreative <quickfoxcreative@gmail.com>
2022-05-04 14:53:34 +0600

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-if User.count == 0    
+unless User.exists?(:user_name => "POS Admin")
     #add admin role
     admin_role = Role.create(:role_name => "System Admin")
-    #add admin user w/ admin role
-    role_id = Role.where("role_name = ?", "System Admin").first.id
+    #get id of admin role
+    role_id = admin_role.id
+    #create admin user
     admin_user = User.create(:user_name => "POS Admin", :email => "quickfoxcreative@gmail.com", :password => "123456")
-    user_id = User.where("user_name = ?", "POS Admin").first.id
+    #get id of admin user
+    user_id = admin_user.id
+    #assign role to user
     RoleUser.create(:user_id => user_id, :role_id => role_id)
 end
 
