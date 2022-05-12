25d105aacae499b5a86a30cae4aa67af51ceb6ac
quickfoxcreative <quickfoxcreative@gmail.com>
2022-05-12 13:53:14 +0600

    exchange rate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery with: :exception
   skip_authorization_check 
+  after_action :get_exchange_rate
   add_flash_types :danger, :info, :warning, :success, :messages
 
 	rescue_from CanCan::AccessDenied do |exception|
@@ -26,6 +27,14 @@ class ApplicationController < ActionController::Base
 	def switch_locale(&action)
 	  locale = params[:locale] || I18n.default_locale
 	  I18n.with_locale(locale, &action)
+	end
+
+	def get_exchange_rate
+		ex_rate_info_xml = %x{ curl -X GET -H "Content-Type: application/json" https://www.nbkr.kg/XML/daily.xml }
+		ex_rate_info_json = Hash.from_xml(ex_rate_info_xml).to_json
+		ex_rates = JSON.parse(ex_rate_info_json)
+		usd_to_som_rate = ex_rates["CurrencyRates"]["Currency"][0]["Value"].gsub(/,/, ".").to_f
+		@exchange_rate = usd_to_som_rate
 	end
 
 end
