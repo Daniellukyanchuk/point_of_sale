d763c15488eff9925bc4493f63fc2f1a4e08d0b8
quickfoxcreative <quickfoxcreative@gmail.com>
2022-05-13 09:07:08 +0600

    added additional client info

---
+++ db/migrate/20220512171911_add_client_fields.rb
@@ -0,0 +1,10 @@
+class AddClientFields < ActiveRecord::Migration[5.2]
+  def change
+    add_column :clients, :last_name, :string
+    add_column :clients, :middle_name, :string
+    add_column :clients, :gender, :string
+    add_column :clients, :date_of_birth, :string
+    add_column :clients, :zip_code, :string
+    add_column :clients, :email, :string    
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_22_080838) do
+ActiveRecord::Schema.define(version: 2022_05_12_171911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,12 @@ ActiveRecord::Schema.define(version: 2022_04_22_080838) do
     t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "last_name"
+    t.string "middle_name"
+    t.string "gender"
+    t.string "date_of_birth"
+    t.string "zip_code"
+    t.string "email"
   end
 
   create_table "inventories", force: :cascade do |t|
