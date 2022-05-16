ff62b0b0d7a976f8beb892742c83b508e64446f9
Daniellukyanchuk <daniellukyanchuk@gmail.com>
2022-05-16 14:02:45 +0600

    added 'categories' column to products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_16_043627) do
+ActiveRecord::Schema.define(version: 2022_05_16_075050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 2022_05_16_043627) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "sales_count", default: 0, null: false
+    t.string "categories"
   end
 
   create_table "purchase_products", force: :cascade do |t|
