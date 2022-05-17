bb788448d2711239b7733be18568653d261d460c
quickfoxcreative <quickfoxcreative@gmail.com>
2022-05-17 10:56:14 +0600

    webhook

---
+++ app/controllers/webhooks_controller.rb
@@ -0,0 +1,33 @@
+class WebhooksController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
+    def create
+        payload = request.body.read
+        sig_header = request.env['HTTP_STRIPE_SIGNATURE']
+        event = nil
+
+        begin
+            event = Stripe::Webhook.construct_event(
+                payload, sig_header, Rails.application.credentials[Rails.env.to_sym][:stripe][:webhook]
+            )
+        rescue JSON::ParseError => e
+            status 400
+            return
+        rescue Stripe::SignatureVerificationError => each
+            # invalid signature
+            puts "Signature error"
+            put e
+            return
+        end
+
+        # handle events
+        case event.type
+        when 'checkout.session.completed'
+            session = event.data.object
+            # @product = Product.find_by...
+            
+        end
+
+        render json: { message: 'success' }
+    end
+end
